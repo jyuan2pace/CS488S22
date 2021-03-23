99738f5e95da0f48888b540c47e27552d381709b
Jun Yuan-Murray <44013483+jyuan2pace@users.noreply.github.com>
2021-03-23 14:45:44 -0400

    ...wrong file. updated

--- projects/proj2/Sender.java
+++ projects/proj2/Sender.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
 import java.net.UnknownHostException;
 public class Sender {
 	final static int ownPort = 7777;
@@ -22,6 +23,20 @@ public class Sender {
 	        File file = new File("./Resource/1.jpg");
 	        FileInputStream fis = new FileInputStream(file);
 			
+	        byte[] data = new byte[1024];
+	        //Socket socket = new Socket(host.getHostAddress(),targetPort);
+	        DatagramSocket datagramSocket = new DatagramSocket(ownPort);
+	        System.out.println("Sender: connection built. about to transfer.");
+	        while(fis.read(data) != -1) 
+	        {
+	        	//socket.getOutputStream().write(data);
+	        	DatagramPacket packet = new DatagramPacket(data, data.length, host, targetPort);
+	        	datagramSocket.send(packet);
+	        }
+	        System.out.println("Sender: finished.");
+	        datagramSocket.close();
+	        
+	        
 	        fis.close();
 	}
 		
